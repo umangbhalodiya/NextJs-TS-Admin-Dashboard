0058b2f65b36b6700047d21d7e4cd2b37f17bec2
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2023-03-30 22:15:38 +0530

    code deployment

---
+++ src/components/footer/FooterAdmin.tsx
@@ -0,0 +1,23 @@
+/*eslint-disable*/
+
+import { Flex, useColorModeValue } from "@chakra-ui/react";
+
+export default function Footer() {
+  const textColor = useColorModeValue("gray.400", "white");
+  return (
+    <Flex
+      zIndex="3"
+      flexDirection={{
+        base: "column",
+        xl: "row",
+      }}
+      alignItems={{
+        base: "center",
+        xl: "start",
+      }}
+      justifyContent="space-between"
+      px={{ base: "30px", md: "50px" }}
+      pb="30px"
+    ></Flex>
+  );
+}
---
+++ src/components/sidebar/components/Content.tsx
@@ -0,0 +1,23 @@
+import { Box, Flex, Stack } from "@chakra-ui/react";
+import Brand from "components/sidebar/components/Brand";
+import Links from "components/sidebar/components/Links";
+import { IRoute } from "types/navigation";
+interface SidebarContentProps {
+  routes: IRoute[];
+}
+
+function SidebarContent(props: SidebarContentProps) {
+  const { routes } = props;
+  return (
+    <Flex direction="column" height="100%" pt="25px" borderRadius="30px">
+      <Brand />
+      <Stack direction="column" mt="8px" mb="auto">
+        <Box ps="20px" pe={{ lg: "16px", "2xl": "16px" }}>
+          <Links routes={routes} />
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
+
+export default SidebarContent;
